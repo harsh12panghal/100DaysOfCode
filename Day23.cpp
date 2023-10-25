c15682de0b46b65f29e1ad0f79517795b7b7e753
Harsh Panghal <102521089+harsh12panghal@users.noreply.github.com>
2023-10-25 20:06:36 +0530

    2373. Largest Local Values in a Matrix

---
+++ Day23.cpp
@@ -0,0 +1,42 @@
+/*
+2373. Largest Local Values in a Matrix
+
+You are given an n x n integer matrix grid.
+
+Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+
+maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+
+Return the generated matrix.
+
+*/
+
+class Solution {
+public:
+
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>> res(n-2, vector<int> (n-2));
+        
+        //find max of 3x3 grid centred around row (i+1) and column (j+1)
+		//for eg:1 starting from (1,1) so (1,1),(1,2),(2,1),(2,2) will be the centre of 3x3 grid-1,2,3,4
+        //respectively
+        for(int i=1; i<=n-2; i++)
+        {
+            for(int j=1; j<=n-2; j++)
+            {
+                int maxi=0;
+                maxi = max({maxi, grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1]});
+                maxi = max({maxi, grid[i][j-1], grid[i][j], grid[i][j+1]});
+                maxi = max({maxi, grid[i+1][j-1], grid[i+1][j], grid[i+1][j+1]});
+                
+               res[i-1][j-1] = maxi;
+            }
+        }
+        return res;
+
+        
+        
+    }
+};
