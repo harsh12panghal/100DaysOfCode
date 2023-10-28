e7d3e4eecb725ea10a3039c8cb6b17ceb9c18740
Harsh Panghal <102521089+harsh12panghal@users.noreply.github.com>
2023-10-28 20:59:09 +0530

    20. Valid Parentheses

---
+++ Day26.cpp
@@ -0,0 +1,40 @@
+/*
+20. Valid Parentheses
+
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+*/
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if (stk.empty()) {
+                    return false;
+                }
+                if (c == ')' && stk.top() == '(') {
+                    stk.pop();
+                } else if (c == '}' && stk.top() == '{') {
+                    stk.pop();
+                } else if (c == ']' && stk.top() == '[') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
