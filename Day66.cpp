774a01facf1bf1238ec6db9bbd8b44f19d8b3e75
Harsh Panghal <102521089+harsh12panghal@users.noreply.github.com>
2023-12-16 19:20:00 +0530

    Create Day66.cpp

---
+++ Day66.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> map;
+
+        if(s.size() != t.size())
+            return false;
+
+        
+        for(int i = 0; i < s.size(); i++){
+            map[s[i]]++;
+        }
+
+        for(int i = 0; i < t.size(); i++){
+            if(map[t[i]] == 0){
+                return false;
+            }
+            map[t[i]]--;
+        }
+        return true;
+    }
+};
